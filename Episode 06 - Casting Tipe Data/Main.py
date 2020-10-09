190ee2913309ca71364989e601d5e1156baedfed
suryaoka21 <suryaokach@gmail.com>
2020-10-09 16:56:38 +0700

    learn

--- Episode 06 - Casting Tipe Data/Main.py
+++ Episode 06 - Casting Tipe Data/Main.py
@@ -4,7 +4,7 @@
 
 ## INTEGER
 print("====INTEGER====")
-data_int = 9;
+data_int = 9
 print("data = ", data_int, ",type =",type(data_int))
 
 data_float = float(data_int)
@@ -16,7 +16,7 @@ print("data = ", data_bool, ",type =",type(data_bool))
 
 ## FLOAT
 print("====FLOAT====")
-data_float = 0;
+data_float = 0
 print("data = ", data_float, ",type =",type(data_float))
 
 data_int = int(data_float) # akan dibulatkan ke bawah
@@ -28,7 +28,7 @@ print("data = ", data_bool, ",type =",type(data_bool))
 
 ## BOOLEAN
 print("====BOOLEAN====")
-data_bool = False;
+data_bool = False
 print("data = ", data_bool, ",type =",type(data_bool))
 
 data_int = int(data_bool) # akan dibulatkan ke bawah
@@ -40,7 +40,7 @@ print("data = ", data_float, ",type =",type(data_float))
 
 ## STRING
 print("====STRING====")
-data_str = "10";
+data_str = "10"
 print("data = ", data_str, ",type =",type(data_str))
 
 data_int    = int(data_str) # string harus angka
